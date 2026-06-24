f71dae1f71a16a7bcd561a38883a05b0005cf05a
Sameen Javed <sameen.javed@tkxel.com>
2026-06-24 18:26:05 +0500

    chore(fulfillment-service): remove boilerplate context test

--- fulfillment-service/src/test/java/com/orderflow/fulfillment/FulfillmentServiceApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.orderflow.fulfillment;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FulfillmentServiceApplicationTests {
-}
